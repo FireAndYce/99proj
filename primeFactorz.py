87754454d4318dc36a4656b8a2d80509d7a9c3ea
FireAndYce <sgorton@lawrenceks.org>
2014-07-23 10:43:11 -0500

    Find Prime Factors
    modified some code

--- primeFactorz.py
+++ primeFactorz.py
@@ -2,19 +2,20 @@
 
 result = ""
 while True:
-        try:
-                i = int(input("What is your number?"))
-        except ValueError:
-                print("that is not an integer")
-                continue
-        else:
-                break
+	try:
+		i = int(input("What is your number?"))
+	except ValueError:
+		print("that is not an integer")
+		continue
+	else:
+		break
 
-s = 2
-for s in range(2,round((i/2))):
-        if (i % s) == 0:
-                print("i:" + str(i) + " s:" + str(s))
-                result += str(s) + ","
-                i = i/s
-                s = 2
+s = 1
+while s <= round(i/2):
+	s+= 1
+	if (i % s) == 0:
+		result += str(s) + ","
+		i = i/s
+		s = 1
+		
 print(result + str(int(i)))
